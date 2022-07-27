40a7788554195597482b9d0280835c8495def60a
Snigdh Kulshreshtha <74950528+Snigdh27@users.noreply.github.com>
2022-07-27 20:03:26 +0530

    Time: 16 ms (83.42%), Space: 7.5 MB (71.96%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m=word1.length(),n=word2.length();
+        int i=m,j=n;
+        
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        int answer=m+n-2*dp[m][n];
+        
+        return answer;
+        
+    }
+};
