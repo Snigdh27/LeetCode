edec4eaebca63b5fef213ab86bfa0cfa7bf36a6e
Snigdh Kulshreshtha <74950528+Snigdh27@users.noreply.github.com>
2022-09-06 23:44:04 +0530

    Time: 20 ms (65.09%), Space: 15 MB (35.53%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        int ans=root->val;
+        switch(ans){
+            case 0:
+                
+                case 1:
+                return ans;
+                case 2:
+                return evaluateTree(root->left) || evaluateTree(root->right);
+            default:
+                return evaluateTree(root->left) && evaluateTree(root->right);
+        }
+    }
+};
