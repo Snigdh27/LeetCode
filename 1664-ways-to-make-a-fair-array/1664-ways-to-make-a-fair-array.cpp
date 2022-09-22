3e8a590721d677093ab112a7951377e788d967d8
Snigdh Kulshreshtha <74950528+Snigdh27@users.noreply.github.com>
2022-09-22 22:48:03 +0530

    Time: 298 ms (25.22%), Space: 94.4 MB (60.64%) - LeetHub

---
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        int evenSum=0,oddSum=0;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            if(i%2==0){
+                evenSum+=nums[i];
+            }
+            else{
+                oddSum+=nums[i];
+            }            
+        }
+        
+        int tempEven=0,tempOdd=0,count=0;
+        
+        for(int i=0;i<n;i++){
+            
+            
+            if(i%2==0){
+                evenSum-=nums[i];
+            }
+            else{
+                oddSum-=nums[i];
+            }
+            
+            //cout<<tempEven<<" "<<tempOdd<<endl;
+            if(oddSum+tempEven==tempOdd+evenSum){
+                count++;
+            }
+            
+            if(i%2==0){
+                tempEven+=nums[i];
+            }
+            else{
+                tempOdd+=nums[i];
+            }
+            
+            
+        }
+        
+        return count;
+        
+    }
+};
